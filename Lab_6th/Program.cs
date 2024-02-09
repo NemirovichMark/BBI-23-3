bf095f5bab06a906823bb1f22bfd18f3205879d7
NemirovichMark <nemirovich.m@misis.ru>
2024-02-09 14:06:39 +0300

    6th lab started

--- Lab_6th/Program.cs
+++ Lab_6th/Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Security.Policy;
 
 namespace Lab_6th
 {
+    struct Animal
+    {
+        private string _name; // string.Empty
+        private int _age; // 0
+        public Animal(string name, int age)
+        {
+            _name = name;
+            _age = age;
+        }
+        public void Print()
+        {
+            Console.WriteLine(_name + ": " + _age);
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Animal cat = new Animal("Cat", 5);
+          //  cat.Print();
+
+            Animal[] animals = new Animal[3];
+            animals[0] = new Animal("Cat", 5);
+            animals[1] = new Animal("Dog", 2);
+            animals[2] = new Animal("Horse", 3);
+
+            for (int i = 0; i < animals.Length; i++)
+            {
+                animals[i].Print();
+            }
         }
     }
 }
