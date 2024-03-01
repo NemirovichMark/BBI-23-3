8b500ee700ff9bf3e6a3a36e7f93bdd730ca6b31
AlexandrKosov123 <159529311+AlexandrKosov123@users.noreply.github.com>
2024-03-01 14:04:44 +0300

    Updated 6laba2lvl_Kosov.cs

--- 6laba_Kosov/6laba2lvlworkingbutnottottalyaccurate_Kosov.cs
+++ 6laba_Kosov/6laba2lvlworkingbutnottottalyaccurate_Kosov.cs
@@ -4,29 +4,40 @@ using System.Linq;
 
 public class Player
 {
-    public string Name { get; set; }
+    public string Name { get; private set; }
     public int PenaltyMins { get; set; }
+
+    public Player(string name, int penaltyMins)
+    {
+        Name = name;
+        PenaltyMins = penaltyMins;
+    }
 }
 
 public class Program
 {
+    public static void PrintPlayers(IEnumerable<Player> players)
+    {
+        foreach (var player in players)
+        {
+            Console.WriteLine($"{player.Name} - {player.PenaltyMins} мин");
+        }
+    }
+
     public static void Main()
     {
         List<Player> players = new List<Player>()
         {
-            new Player() { Name = "Krosh", PenaltyMins = 10 },
-            new Player() { Name = "Pin", PenaltyMins = 5 },
-            new Player() { Name = "Ezhik", PenaltyMins = 10 },
-            new Player() { Name = "Losjash", PenaltyMins = 2 },
-            new Player() { Name = "Kopatich", PenaltyMins = 5 },
-            new Player() { Name = "Barash", PenaltyMins = 2 },
+            new Player("Krosh", 10),
+            new Player("Pin", 5),
+            new Player("Ezhik", 10),
+            new Player("Losjash", 2),
+            new Player("Kopatich", 5),
+            new Player("Barash", 2),
         };
 
         var candidates = players.Where(p => p.PenaltyMins != 10).OrderBy(p => p.PenaltyMins).ToList();
 
-        foreach (var player in candidates)
-        {
-            Console.WriteLine($"{player.Name} - {player.PenaltyMins} мин");
-        }
+        PrintPlayers(candidates);
     }
 }
