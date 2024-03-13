44c9b14f49b574ce14cd6bbdf600169cc4f50f0c
ffonarev <161751668+ffonarev@users.noreply.github.com>
2024-03-13 23:59:13 +0300

    Поправил программу, посмотрите пожалуйста

--- lab1/lab1n2/Program.cs
+++ lab1/lab1n2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace lab2n2
 {
@@ -8,18 +8,45 @@ namespace lab2n2
         private double _res1;
         private double _res2;
         private double _res3;
-        public double bestres;
+        private double _bestres;
+        
+
         public Jumps(string surname, double res1, double res2, double res3)
         {
             _surname = surname;
             _res1 = res1;
             _res2 = res2;
             _res3 = res3;
-            bestres = 0;
+            if (res1 > res2)
+            {
+                if (res1 > res3)
+                {
+
+                    _bestres = res1;
+                }
+                else
+                {
+                    _bestres = res3;
+                }
+            }
+            else
+            {
+                if (res2 > res3)
+                {
+
+                    _bestres = res2;
+                }
+                else
+                {
+                    _bestres = res3;
+                }
+            }
+            _bestres = bestres;
         }
         public double res1 => _res1;
         public double res2 => _res2;
         public double res3 => _res3;
+        public double bestres => _bestres;
         public void Print()
         {
             Console.WriteLine("{0,-7} | {1,-3}", _surname, bestres);
@@ -27,40 +54,6 @@ namespace lab2n2
     }
     internal class Program
     {
-        static void bestof3(Jumps[] contestants)
-        {
-            
-            for(int i = 0; i < contestants.Length; i++)
-            {
-
-                if(contestants[i].res1 > contestants[i].res2)
-                {
-                    if (contestants[i].res1 > contestants[i].res3)
-                    {
-                      
-                        contestants[i].bestres = contestants[i].res1;
-                    }
-                    else
-                    {
-                        contestants[i].bestres = contestants[i].res3;
-                    }
-                }
-                else
-                {
-                    if (contestants[i].res2 > contestants[i].res3)
-                    {
-
-                        contestants[i].bestres = contestants[i].res2;
-                    }
-                    else
-                    {
-                        contestants[i].bestres = contestants[i].res3;
-                    }
-                }
-
-            }
-            
-        }
         static void Main(string[] args)
         {
             Jumps[] contestants = new Jumps[5]
@@ -71,9 +64,6 @@ namespace lab2n2
                 new Jumps("Grey", 3, 2, 4),
                 new Jumps("Black", 7, 9, 6)
             };
-
-            
-            bestof3(contestants);
             Console.WriteLine("Сортированный список");
             for (int i = 0; i < contestants.Length; i++)
             {
