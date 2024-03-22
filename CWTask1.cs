0d52534db031b65f56857824da5f5bbd3a1fef70
KalininArtem2 <164404510+KalininArtem2@users.noreply.github.com>
2024-03-22 14:15:04 +0300

    Add files via upload

---
+++ CWTask1.cs
@@ -0,0 +1,80 @@
+﻿using ConsoleApp3.Series;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class program { }
+    internal class list { }
+    namespace Series
+    {
+        struct Series
+        {
+            public string Title { get; set; }
+            public double AverageEpisodeLength { get; set; }
+            public string Description { get; set; }
+            public bool IsWatched { get; set; }
+
+            public Series(string title, double averageEpisodeLength)
+            {
+                Title = title;
+                AverageEpisodeLength = averageEpisodeLength;
+                Description = $"Для сериала {Title} описание не задано.";
+                IsWatched = false;
+            }
+
+            public void ChangeDescription(string newDescription)
+            {
+                if (newDescription.Length < 20 || newDescription.Length > 200)
+                {
+                    Console.WriteLine("Описание должно быть длиной от 20 до 200 символов.");
+                }
+                else
+                {
+                    Description = newDescription;
+                }
+            }
+
+            public void ChangeWatchedStatus()
+            {
+                IsWatched = true;
+            }
+
+            public void PrintInfo()
+            {
+                Console.WriteLine($"Название: {Title}");
+                Console.WriteLine($"Средняя продолжительность серии: {AverageEpisodeLength} минут");
+                Console.WriteLine($"Описание: {Description}");
+                Console.WriteLine($"Просмотрен: {IsWatched}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        List seriesList = new List {
+new Series("Игра престолов", 55),
+new Series("Во все тяжкие", 45),
+new Series("Друзья", 25),
+new Series("Офис", 20),
+new Series("Шерлок", 60)
+};
+
+        seriesList.Sort((s1, s2) => s1.Title.CompareTo(s2.Title));
+
+        Console.WriteLine("Список сериалов:");
+        Console.WriteLine("---------------------------------------------------------------------------------------------------");
+        Console.WriteLine("| Название | Средняя продолжительность серии | Описание | Просмотрен |");
+        Console.WriteLine("---------------------------------------------------------------------------------------------------");
+        foreach (Series series in seriesList)
+        {
+            series.PrintInfo();
+        }
+    }
+}
---
+++ CWTask2.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Deployment.Internal;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+            namespace WatchingApp
+    {
+        public abstract class Watching
+        {
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public string Status { get; set; }
+
+            public Watching(string title, string description, string status)
+            {
+                Title = title;
+                Description = description;
+                Status = status;
+            }
+        }
+
+        public class Movie : Watching
+        {
+            public int AgeRating { get; set; }
+            public int Duration { get; set; }
+
+            public Movie(string title, string description, string status, int ageRating, int duration) : base(title, description, status)
+            {
+                AgeRating = ageRating;
+                Duration = duration;
+                Description = $"Для фильма {Title} описание не задано";
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+               
+                List watchings = new List {
+new Movie("Фильм 1", "Описание фильма 1", "Не просмотрен", 10, 124),
+new Movie("Фильм 2", "Описание фильма 2", "Просмотрен", 14, 110),
+new Movie("Фильм 3", "Описание фильма 3", "Не просмотрен", 19, 181),
+new Movie("Фильм 4", "Описание фильма 4", "Просмотрен", 15, 120),
+new Movie("Фильм 5", "Описание фильма 5", "Не просмотрен", 16, 130)
+};
+
+                watchings.Sort((x, y) => x.Title.CompareTo(y.Title));
+
+                
+                Console.WriteLine("Список фильмов:");
+            Console.WriteLine("-");
+        }
+    }
+
+
+
