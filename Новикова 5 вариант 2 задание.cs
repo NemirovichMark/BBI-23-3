66c182caf095d7de2beb97a29c12df5bca8d4253
Uliana-Novikova <159529899+Uliana-Novikova@users.noreply.github.com>
2024-03-22 14:15:36 +0300

    Add files via upload

---
+++ Новикова 5 вариант 2 задание.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    abstract class Book
+    {
+        protected string title;
+        protected int isbn;
+        protected string author;
+        protected int year;
+        protected double price;
+
+        public Book(string title, int isbn, string author, int year, double price)
+        {
+            this.title = title;
+            this.isbn = isbn;
+            this.author = author;
+            this.year = year;
+            this.price = price;
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine("Название: {0}", title);
+            Console.WriteLine("ISBN: {0}", isbn);
+            Console.WriteLine("Автор: {0}", author);
+            Console.WriteLine("Год издания: {0}", year);
+            Console.WriteLine("Цена: {0}", price);
+            Console.WriteLine();
+        }
+
+        public string GetAuthor()
+        {
+            return author;
+        }
+
+        public int GetYear()
+        {
+            return year;
+        }
+
+        public abstract double CalculatePrice()
+        ;
+    }
+
+    class PaperBook : Book
+    {
+        private bool hardCover;
+
+        public PaperBook(string title, int isbn, string author, int year, double price, bool hardCover) : base(title, isbn, author, year, price)
+        {
+            this.hardCover = hardCover;
+        }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine("Твердый переплет: {0}", hardCover ? "Да" : "Нет");
+        }
+
+        public override double CalculatePrice()
+        {
+            return price * 1.2;
+        }
+    }
+
+    class ElectronicBook : Book
+    {
+        private string format;
+
+        public ElectronicBook(string title, int isbn, string author, int year, double price, string format) : base(title, isbn, author, year, price)
+        {
+            this.format = format;
+        }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine("Формат: {0}", format);
+        }
+
+        public override double CalculatePrice()
+        {
+            return price * 0.8;
+        }
+    }
+
+    class AudioBook : Book
+    {
+        private bool studioRecording;
+
+        public AudioBook(string title, int isbn, string author, int year, double price, bool studioRecording) : base(title, isbn, author, year, price)
+        {
+            this.studioRecording = studioRecording;
+        }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine("Студийная запись: {0}", studioRecording ? "Да" : "Нет");
+        }
+
+        public override double CalculatePrice()
+        {
+            return price * 0.9;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            PaperBook[] paperBooks = new PaperBook[]
+            {
+            new PaperBook("Бумажная книга1", 111, "Автор1", 2000, 20.5, true),
+            new PaperBook("Бумажная книга2", 222, "Автор2", 1996, 18.75, false),
+            new PaperBook("Бумажная книга3", 333, "Автор3", 2010, 23.0, true),
+            new PaperBook("Бумажная книга4", 444, "Автор4", 1985, 15.25, false),
+            new PaperBook("Бумажная книга5", 555, "Автор5", 2021, 27.99, true)
+            };
+
+            ElectronicBook[] electronicBooks = new ElectronicBook[]
+            {
+            new ElectronicBook("Электронная книга1", 101, "Автор1", 2005, 15.99, "pdf"),
+            new ElectronicBook("Электронная книга2", 202, "Автор2", 1999, 12.5, "fb2"),
+            new ElectronicBook("Электронная книга3", 303, "Автор3", 2014, 17.0, "epub"),
+            new ElectronicBook("Электронная книга4", 404, "Автор4", 1989, 10.75, "pdf"),
+            new ElectronicBook("Электронная книга5", 505, "Автор5", 2018, 20.25, "fb2")
+            };
+
+            AudioBook[] audioBooks = new AudioBook[]
+            {
+            new AudioBook("Аудиокнига1", 1111, "Автор1", 2008, 22.0, true),
+            new AudioBook("Аудиокнига2", 2222, "Автор2", 1995, 18.5, false),
+            new AudioBook("Аудиокнига3", 3333, "Автор3", 2012, 25.75, true),
+            new AudioBook("Аудиокнига4", 4444, "Автор4", 1980, 14.99, false),
+            new AudioBook("Аудиокнига5", 5555, "Автор5", 2016, 19.99, true)
+            };
+            Console.WriteLine("   Бумажные книги   ");
+            PaperBook[] sortedPaperBooks = Sort(paperBooks);
+            foreach (var book in sortedPaperBooks)
+            {
+                book.DisplayInfo();
+            }
+
+            Console.WriteLine("   Электронные книги   ");
+            ElectronicBook[] sortedElectronicBooks = Sort(electronicBooks);
+            foreach (var book in sortedElectronicBooks)
+            {
+                book.DisplayInfo();
+            }
+
+            Console.WriteLine("   Аудиокниги   ");
+            var sortedAudioBooks = Sort(audioBooks);
+            foreach (var book in sortedAudioBooks)
+            {
+                book.DisplayInfo();
+            }
+
+        }
+
+        static T Sort<T>(T array) where T : Book
+        {
+            T temp;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j].Price < array[j + 1].Price)
+                    {
+                        temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+    }
+
+}
---
+++ Новикова вариант 5 задание 1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    struct Book
+    {
+        private string title;
+        private int isbn;
+        private string author;
+        private int year;
+
+        public Book(string title, int isbn, string author, int year)
+        {
+            this.title = title;
+            this.isbn = isbn;
+            this.author = author;
+            this.year = year;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Название: {0}", title);
+            Console.WriteLine("ISBN: {0}", isbn);
+            Console.WriteLine("Автор: {0}", author);
+            Console.WriteLine("Год издания: {0}", year);
+            Console.WriteLine();
+        }
+
+        public string GetAuthor()
+        {
+            return author;
+        }
+
+        public int GetYear()
+        {
+            return year;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Book[] books = new Book[10];
+
+            books[0] = new Book("Книга1", 12345, "Автор1", 2000);
+            books[1] = new Book("Книга2", 32135, "Автор2", 1990);
+            books[2] = new Book("Книга3", 94135, "Автор3", 1987);
+            books[3] = new Book("Книга4", 38635, "Автор4", 1902);
+            books[4] = new Book("Книга5", 32005, "Автор5", 2007);
+            books[5] = new Book("Книга6", 32609, "Автор1", 2022);
+            books[6] = new Book("Книга7", 32905, "Автор2", 1901);
+            books[7] = new Book("Книга8", 98679, "Автор3", 1900);
+            books[8] = new Book("Книга9", 84782, "Автор4", 1970);
+            books[9] = new Book("Книга10", 30981, "Автор5", 1940);
+
+            Console.WriteLine("Все книги:");
+            foreach (Book book in books)
+            {
+                book.DisplayInfo();
+            }
+
+            Console.Write("Введите имя автора: ");
+            string authorName = Console.ReadLine();
+            Console.WriteLine("Книги автора {0}:", authorName);
+            foreach (Book book in books)
+            {
+                if (book.GetAuthor() == authorName)
+                {
+                    book.DisplayInfo();
+                }
+            }
+
+            Console.Write("Введите век: ");
+            int century = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Книги из {0} века:", century);
+            foreach (Book book in books)
+            {
+                if ((book.GetYear() / 100) + 1 == century)
+                {
+                    book.DisplayInfo();
+                }
+            }
+        }
+
+    }
+}
