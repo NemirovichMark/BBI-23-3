744c319165b8051388d6ea774f35940efb562d7e
PihterovaNatalia <159529985+PihterovaNatalia@users.noreply.github.com>
2024-03-22 14:17:21 +0300

    Add files via upload

---
+++ CWTask1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+
+
+namespace ConsoleApp5
+
+{
+
+    class GuessGame
+
+    {
+
+        private Int32 record;
+        private Int32 currentNumber;
+
+        public GuessGame(Int32 newNumber)
+
+        {
+          record = Int32.MaxValue;
+          currentNumber = newNumber;
+
+        }
+
+        public void game()
+
+        {
+
+            Int32 select = -100;
+            Int32 cnt = 0;
+
+            while (select != currentNumber)
+
+            {
+             Console.Write("Введите число: ");
+
+               select = Convert.ToInt32(Console.ReadLine());
+
+                if (select != currentNumber)
+
+                {
+                 Console.WriteLine("Это не то число.");
+                 }
+
+                cnt += 1;
+
+            }
+             if (record > cnt)
+
+            {
+              Console.WriteLine("Новый рекорд");
+              record = cnt;
+
+            }
+
+            gameover();
+
+        }
+
+        void gameover()
+
+        {
+         Console.WriteLine("Игра окончена");
+         Console.WriteLine("Загаданное число: {0}", currentNumber);
+         Console.WriteLine("Рекорд: {0}", record);
+         Console.WriteLine("Попробовать снова?");
+
+        }
+
+    }
+       class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+         Random rand = new Random();
+         Console.WriteLine("Добро пожаловать в игру");
+         Console.WriteLine("Хотите ли сыграть в угадай число?");
+
+
+         string select = Console.ReadLine();
+
+        while (select == "да")
+
+            {
+               GuessGame g = new GuessGame(rand.Next(0, 10));
+               g.game();
+               select = Console.ReadLine();
+
+            }
+
+
+
+        }
+
+    }
+
+}
+
+
---
+++ CWTask2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+
+  abstract class Game
+{
+    public abstract void StartGame();
+}
+class NumberGame : Game
+{
+    public override void StartGame()
+    {
+     Console.WriteLine("Давайте сыграем в игру. Введите диапазон чисел (от и до):");
+
+     int from = Convert.ToInt32(Console.ReadLine());
+     int to = Convert.ToInt32(Console.ReadLine());
+
+        if (to - from < 2  from < 0  to < 0)
+        {
+         Console.WriteLine("Ошибка. Проверьте условия.");
+            return;
+        }
+
+
+
+    }
+}
+
