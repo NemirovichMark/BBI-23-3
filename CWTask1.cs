84d6c3f22956555ddc5862d6571c5829a4b2f2f0
LomakinaJulia <159529945+LomakinaJulia@users.noreply.github.com>
2024-03-22 14:12:49 +0300

    Add files via upload

---
+++ CWTask1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+struct Point
+{
+    private int X;
+    private int Y;
+
+    public Point(int[] coordinates)
+    {
+        X = coordinates[0];
+        Y = coordinates[1];
+    }
+
+    public double DistanceTo(Point dist)
+    {
+        return Math.Sqrt(Math.Pow(dist.X - X, 2) + Math.Pow(dist.Y - Y, 2));
+    }
+
+    public void DisplayInfo(Point other)
+    {
+        double distance = DistanceTo(other);
+        Console.WriteLine("Точка 1: X = {0}, Y = {1}", X, Y);
+        Console.WriteLine("Точка 2: X = {0}, Y = {1}", other.X, other.Y);
+        Console.WriteLine("Расстояние между точками: {0:F2}", distance);
+        Console.WriteLine();
+    }
+
+    public int GetX()
+    {
+        return X;
+    }
+
+    public int GetY()
+    {
+        return Y;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Point[] points = new Point[3];
+        points[0] = new Point(new int[] { 0, 0 });
+        points[1] = new Point(new int[] { 4, 4 });
+        points[2] = new Point(new int[] { -5, -1 });
+
+        for (int i = 0; i < points.Length - 1; i++)
+        {
+            for (int j = i + 1; j < points.Length; j++)
+            {
+                points[i].DisplayInfo(points[j]);
+            }
+        }
+    }
+}
+
+
---
+++ CWTask2.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+abstract class Profession
+{
+    private string Title;
+    protected double ExperienceBonus;
+
+    public abstract double CalculateSalary();
+
+    public Profession(string title, double experienceBonus)
+    {
+        Title = title;
+        ExperienceBonus = experienceBonus;
+    }
+
+    public string GetTitle()
+    {
+        return Title;
+    }
+
+    public static void GnomeSortBySalary(List<Profession> professions)
+    {
+        int i = 0;
+        while (i < professions.Count)
+        {
+            if (i == 0 || professions[i].CalculateSalary() <= professions[i - 1].CalculateSalary())
+            {
+                i++;
+            }
+            else
+            {
+                Profession temp = professions[i];
+                professions[i] = professions[i - 1];
+                professions[i - 1] = temp;
+                i--;
+            }
+        }
+    }
+}
+
+class Fireman : Profession
+{
+    private double DangerBonus;
+
+    public Fireman(string title, double experienceBonus, double dangerBonus) : base(title, experienceBonus)
+    {
+        DangerBonus = dangerBonus;
+    }
+
+    public override double CalculateSalary()
+    {
+        return 2000 + ExperienceBonus * 100 + DangerBonus * 500;
+    }
+}
+
+class Engineer : Profession
+{
+    private int Category;
+
+    public Engineer(string title, double experienceBonus, int category) : base(title, experienceBonus)
+    {
+        Category = category;
+    }
+
+    public override double CalculateSalary()
+    {
+        return 2500 + ExperienceBonus * 150 + Category * 300;
+    }
+}
+
+class Scientist : Profession
+{
+    private string Degree;
+
+    public Scientist(string title, double experienceBonus, string degree) : base(title, experienceBonus)
+    {
+        Degree = degree;
+    }
+
+    public override double CalculateSalary()
+    {
+        return 3000 + ExperienceBonus * 200 + (Degree == "PhD" ? 1000 : 500);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Profession> workers = new List<Profession>
+        {
+            new Fireman("Firefighter1", 15, 3),
+            new Fireman("Firefighter2", 8, 4),
+            new Fireman("Firefighter3", 11, 1),
+            new Fireman("Firefighter4", 9, 9),
+            new Fireman("Firefighter5", 7, 4),
+            new Engineer("Engineer1", 11, 2),
+            new Engineer("Engineer2", 19, 7),
+            new Engineer("Engineer3", 15, 7),
+            new Engineer("Engineer4", 13, 9),
+            new Engineer("Engineer5", 11, 7),
+            new Scientist("Researcher1", 15, "MSc"),
+            new Scientist("Researcher2", 11, "PhD"),
+            new Scientist("Professor1", 20, "PhD"),
+            new Scientist("Professor2", 21, "PhD"),
+            new Scientist("Professor3", 19, "MSc")
+        };
+
+        Profession.GnomeSortBySalary(workers);
+
+        Console.WriteLine("Firemen:");
+        Console.WriteLine("{0,-15} {1,-15} {2,-15}", "Title", "Profession", "Salary");
+        foreach (var worker in workers.FindAll(w => w is Fireman))
+        {
+            Console.WriteLine("{0,-15} {1,-15} {2,-15}", worker.GetTitle(), worker.GetType().Name, worker.CalculateSalary());
+        }
+
+        Console.WriteLine("\nEngineers:");
+        Console.WriteLine("{0,-15} {1,-15} {2,-15}", "Title", "Profession", "Salary");
+        foreach (var worker in workers.FindAll(w => w is Engineer))
+        {
+            Console.WriteLine("{0,-15} {1,-15} {2,-15}", worker.GetTitle(), worker.GetType().Name, worker.CalculateSalary());
+        }
+
+        Console.WriteLine("\nScientists:");
+        Console.WriteLine("{0,-15} {1,-15} {2,-15}", "Title", "Profession", "Salary");
+        foreach (var worker in workers.FindAll(w => w is Scientist))
+        {
+            Console.WriteLine("{0,-15} {1,-15} {2,-15}", worker.GetTitle(), worker.GetType().Name, worker.CalculateSalary());
+        }
+
+        Console.WriteLine("\nAll Professions:");
+        Console.WriteLine("{0,-15} {1,-15} {2,-15}", "Title", "Profession", "Salary");
+        for (int i=0; i<workers.LongCount(); i++)
+        {
+            Console.WriteLine("{0,-15} {1,-15} {2,-15}", workers[i].GetTitle(), workers[i].GetType().Name, workers[i].CalculateSalary());
+        }
+    }
+}
+
+
