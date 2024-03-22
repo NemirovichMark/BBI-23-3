bbb3373219946420baf7d0f15a843082ee228b46
AlminaUtnasunova <159530330+AlminaUtnasunova@users.noreply.github.com>
2024-03-22 14:26:27 +0300

    Add files via upload

---
+++ CWTask1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+struct Rectangle
+{
+    private double Length;
+    private double Width;
+
+    public Rectangle(double length, double width)
+    {
+        Length = length;
+        Width = width;
+    }
+
+    public double CalculateS()
+    {
+        return Length * Width;
+    }
+
+    public double CalculateP()
+    {
+        return 2 * (Length + Width);
+    }
+
+    public void Compare(Rectangle otherRectangle)
+    {
+        Console.WriteLine("Results:");
+        Console.WriteLine(" ");
+
+        Console.WriteLine("Rectangle A - Length: " + Length + ", Width: " + Width);
+        Console.WriteLine("Rectangle B - Length: " + otherRectangle.Length + ", Width: " + otherRectangle.Width);
+
+        if (Length > otherRectangle.Length)
+            Console.WriteLine("Rectangle A is longer than Rectangle B.");
+        else
+            Console.WriteLine("Rectangle A is shorter than Rectangle B.");
+
+        if (Width > otherRectangle.Width)
+            Console.WriteLine("Rectangle A is wider than Rectangle B.");
+        else
+            Console.WriteLine("Rectangle A is narrower than Rectangle B.");
+
+        if (CalculateS() > otherRectangle.CalculateS())
+            Console.WriteLine("Rectangle A has a greater area.");
+        else
+            Console.WriteLine("Rectangle B has a greater area.");
+
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Rectangle[] rectangle = new Rectangle[3];
+
+        rectangle[0] = new Rectangle(5, 10);
+        rectangle[1] = new Rectangle(8, 6);
+        rectangle[2] = new Rectangle(7, 9);
+
+        Console.WriteLine("Rectangle Comparison:");
+        Console.WriteLine(" ");
+
+        for (int i = 0; i < rectangle.Length; i++)
+        {
+            for (int j = i + 1; j < rectangle.Length; j++)
+            {
+                Console.WriteLine($"Comparing Rectangle {i + 1} = Rectangle A and Rectangle {j + 1} = Rectangle B:");
+                rectangle[i].Compare(rectangle[j]);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ CWTask2.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+abstract class Embrasure
+{
+    protected string Name;
+    protected double Width;
+    protected double Length;
+    protected double Thickness;
+
+    public Embrasure(string name, double width, double length, double thickness)
+    {
+        Name = name;
+        Width = width;
+        Length = length;
+        Thickness = thickness;
+    }
+
+    public abstract double CalculateInstallationCost();
+}
+
+class Window : Embrasure
+{
+    private int NumberOfLayers;
+
+    public Window(string name, double width, double length, double thickness, int numberOfLayers): base(name, width, length, thickness)
+    {
+        NumberOfLayers = numberOfLayers;
+    }
+
+    public override double CalculateInstallationCost()
+    {
+        return Width * Length * Thickness * NumberOfLayers * 20;
+    }
+}
+
+class Door : Embrasure
+{
+    private bool Pattern;
+    private bool Glass;
+
+    public Door(string name, double width, double length, double thickness, bool pattern, bool glass): base(name, width, length, thickness)
+    {
+        Pattern = pattern;
+        Glass = glass;
+    }
+
+    public override double CalculateInstallationCost()
+    {
+        double cost = Width * Length * Thickness * 30;
+
+        if (Pattern)
+        {
+            cost += 100;
+        }
+        if (Glass)
+        {
+            cost += 200;
+        }
+
+        return cost;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Embrasure[] embrasures = new Embrasure[10];
+
+        for (int i = 0; i < embrasures.Length - 1; i++)
+        {
+            for (int j = i + 1; j < embrasures.Length; j++)
+            {
+                if (embrasures[i].CalculateInstallationCost() > embrasures[j].CalculateInstallationCost())
+                {
+                    Embrasure temp = embrasures[i];
+                    embrasures[i] = embrasures[j];
+                    embrasures[j] = temp;
+                }
+            }
+        }
+
+        Console.WriteLine("Windows:");
+        Console.WriteLine("  ");
+        foreach (var item in embrasures)
+        {
+            if (item is Window)
+            {
+                Console.WriteLine($"{item.Name,-10} {item.Width,-10} {item.Length,-10} {item.Thickness,-10} {item.CalculateInstallationCost()}");
+            }
+        }
+
+        Console.WriteLine("Doors:");
+        Console.WriteLine("  ");
+        foreach (var item in embrasures)
+        {
+            if (item is Door)
+            {
+                Console.WriteLine($"{item.Name,-10} {item.Width,-10} {item.Length,-10} {item.Thickness,-10} {item.CalculateInstallationCost()}");
+            }
+        }
+    }
+}
