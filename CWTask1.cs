5a00d1d744c136cbe0745e3a983c5e601b7062d6
KirchuKsenia <159530394+KirchuKsenia@users.noreply.github.com>
2024-03-22 13:58:19 +0300

    Вариант 14

---
+++ CWTask1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+struct GuessGame
+{
+    private int secretNumber;
+    private bool numberGuessed;
+    private int attempts;
+    private int record;
+
+    public void Game()
+    {
+        secretNumber = new Random().Next(0, 11);
+        numberGuessed = false;
+        attempts = 0;
+        record = int.MaxValue;
+    }
+
+    public void StartGame()
+    {
+        while (!numberGuessed)
+        {
+            Console.Write("Угадайте число от 0 до 10: ");
+            int guess = Convert.ToInt32(Console.ReadLine());
+            attempts++;
+
+            if (guess == secretNumber)
+            {
+                numberGuessed = true;
+                if (attempts < record)
+                {
+                    record = attempts;
+                }
+            }
+            else
+            {
+                if (guess < secretNumber)
+                {
+                    Console.WriteLine("Загаданное число больше.");
+                }
+                else
+                {
+                    Console.WriteLine("Загаданное число меньше.");
+                }
+            }
+        }
+
+        Console.WriteLine($"Поздравляем! Вы угадали число {secretNumber} за {attempts} попыток.");
+        Console.WriteLine($"Рекорд: {record} попыток.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        GuessGame game = new GuessGame();
+        game.Game();
+        game.StartGame();
+    }
+}
