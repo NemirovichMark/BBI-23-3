d5a59f25031e69b1e2c12b97999223152d8a2d14
RachkovskayaLiza <160212585+RachkovskayaLiza@users.noreply.github.com>
2024-03-22 14:08:13 +0300

    Add files via upload

---
+++ Task_1.cs
@@ -0,0 +1,100 @@
+﻿
+namespace number1
+{
+
+
+
+    public struct Rectangle
+    {
+        private double length;
+        private double width;
+
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+
+
+        public double Area()
+        {
+            return length * width;
+        }
+
+
+
+        public double Perimeter()
+        {
+            return 2 * (length + width);
+        }
+
+
+
+        public void Compare(Rectangle other)
+        {
+            Console.WriteLine($"Сравнение прямоугольников:");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine($"Rectangle 1: {length} x {width}");
+            Console.WriteLine($"Rectangle 2: {other.length} x {other.width}");
+
+            if (length > other.length)
+                Console.WriteLine("Первый прямоугольник больше по длине.");
+            else if (length < other.length)
+                Console.WriteLine("Второй прямоугольник больше по длине .");
+            else
+                Console.WriteLine(" Прямоугольники одиноковой длинны .");
+
+            if (width > other.width)
+                Console.WriteLine("Первый прямоугольник шире .");
+            else if (width < other.width)
+                Console.WriteLine(" Второй прямоугольник шире .");
+            else
+                Console.WriteLine(" Прямоугольники одинаковые по ширине.");
+
+            double area1 = Area();
+            double area2 = other.Area();
+
+            if (area1 > area2)
+                Console.WriteLine("Первый прямоугольник больше по площади .");
+            else if (area1 < area2)
+                Console.WriteLine("Второй прямоугольник больше по площади.");
+            else
+                Console.WriteLine(" Прямоугольники одинаковой площади.");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Rectangle rectangle1 = new Rectangle(5, 10);
+            Rectangle rectangle2 = new Rectangle(8, 6);
+            Rectangle rectangle3 = new Rectangle(7, 7);
+
+            CompareRectangles(rectangle1, rectangle2);
+            Console.WriteLine();
+
+            CompareRectangles(rectangle1, rectangle3);
+            Console.WriteLine();
+
+            CompareRectangles(rectangle2, rectangle3);
+        }
+
+        static void CompareRectangles(Rectangle rect1, Rectangle rect2)
+        {
+            rect1.Compare(rect2);
+            Console.WriteLine();
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
---
+++ Task_2.cs
@@ -0,0 +1,51 @@
+﻿namespase task2
+{
+
+}
+
+
+
+abstract class Embrasure
+{
+    public string Name { get; set; }
+    public double Width { get; set; }
+    public double Height { get; set; }
+    public double Thickness { get; set; }
+
+    public abstract double Cost();
+}
+
+class Window : Embrasure
+{
+    public int Sloi { get; set; }
+
+    public override double Cost()
+    {
+      
+        return Width * Height * Thickness * Sloi;
+    }
+}
+
+class Door : Embrasure
+{
+    public bool Usor { get; set; }
+    public bool Glass { get; set; }
+
+    public override double Cost()
+    {
+        
+        return Width * Height * Thickness;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Window[] windows = new Window[5];
+        Door[] doors = new Door[5];
+
+        
+       
+    }
+}
