4fe930a74efae4e3924a393bd1831a0d21a75967
YastrebovaAleksandra <159530005+YastrebovaAleksandra@users.noreply.github.com>
2024-03-27 12:28:52 +0300

    Add files via upload

---
+++ CWTask1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+struct Dot
+{
+    public int x;
+    public int y;
+    public int z;
+}
+
+struct Vector
+{
+    public Dot Point1;
+    public Dot Point2;
+
+    public Vector(int[,] matrix)
+    {
+        Point1 = new Dot { x = matrix[0, 0], y = matrix[0, 1], z = matrix[0, 2] };
+        Point2 = new Dot { x = matrix[1, 0], y = matrix[1, 1], z = matrix[1, 2] };
+    }
+
+    public double CalculateLength()
+    {
+        int dx = Point2.x - Point1.x;
+        int dy = Point2.y - Point1.y;
+        int dz = Point2.z - Point1.z;
+
+        return Math.Sqrt(dx * dx + dy * dy + dz * dz);
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine("Координаты первой точки: ({0}, {1}, {2}", Point1.x, Point1.y, Point1.z);
+        Console.WriteLine("Координаты второй точки: ({0}, {1}, {2}", Point2.x, Point2.y, Point2.z);
+        Console.WriteLine("Длина вектора: {0}", CalculateLength());
+        Console.WriteLine();
+    }
+}
+class Programm
+{
+    static void Main()
+    {
+        Vector[] vectors = new Vector[5];
+        vectors[0] = new Vector(new int[,] { { 1, 2, 3 }, { 4, 5, 6 } });
+        vectors[1] = new Vector(new int[,] { { 2, 3, 4 }, { 5, 6, 7 } });
+        vectors[2] = new Vector(new int[,] { { 3, 4, 5 }, { 6, 7, 8 } });
+        vectors[3] = new Vector(new int[,] { { 4, 5, 6 }, { 7, 8, 9 } });
+        vectors[4] = new Vector(new int[,] { { 5, 6, 7 }, { 8, 9, 10 } });
+
+        Array.Sort(vectors, (v1, v2) => v2.CalculateLength().CompareTo(v1.CalculateLength()));
+
+        foreach (var vector in vectors)
+        {
+            vector.PrintInfo();
+        }
+    }
+}
---
+++ CWTask2.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+abstract class Shape
+{
+    public abstract double Volume { get; }
+
+    public abstract double CalculateVolume();
+
+    public abstract void PrintInfo();
+}
+
+class Sphere : Shape
+{
+    public double Radius { get; }
+
+    public override double Volume => CalculateVolume();
+
+    public Sphere(double radius)
+    {
+        Radius = radius;
+    }
+
+    public override double CalculateVolume()
+    {
+        return 4 * Math.PI * Math.Pow(Radius, 3) / 3;
+    }
+
+    public override void PrintInfo()
+    {
+        Console.WriteLine($"Шар с радиусом {Radius}");
+    }
+}
+
+class Cube : Shape
+{
+    public double SideLength { get; }
+
+    public override double Volume => CalculateVolume();
+
+    public Cube(double sideLength)
+    {
+        SideLength = sideLength;
+    }
+
+    public override double CalculateVolume()
+    {
+        return Math.Pow(SideLength, 3);
+    }
+
+    public override void PrintInfo()
+    {
+        Console.WriteLine($"Куб со стороной {SideLength}");
+    }
+}
+
+class Cylinder : Shape
+{
+    public double Radius { get; }
+    public double Height { get; }
+
+    public override double Volume => CalculateVolume();
+
+    public Cylinder(double radius, double height)
+    {
+        Radius = radius;
+        Height = height;
+    }
+
+    public override double CalculateVolume()
+    {
+        return Math.PI * Math.Pow(Radius, 2) * Height;
+    }
+
+    public override void PrintInfo()
+    {
+        Console.WriteLine($"Цилиндр с радиусом {Radius} и высотой {Height}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Shape[] spheres = new Shape[]
+        {
+            new Sphere(3),
+            new Sphere(2),
+            new Sphere(4),
+            new Sphere(1),
+            new Sphere(5)
+        };
+
+        Array.Sort(spheres, (s1, s2) => s2.Volume.CompareTo(s1.Volume));
+        Console.WriteLine("Шары:");
+        foreach (var sphere in spheres)
+        {
+            sphere.PrintInfo();
+        }
+
+        Shape[] cubes = new Shape[]
+        {
+            new Cube(3),
+            new Cube(2),
+            new Cube(4),
+            new Cube(1),
+            new Cube(5)
+        };
+
+        Array.Sort(cubes, (c1, c2) => c2.Volume.CompareTo(c1.Volume));
+        Console.WriteLine("Кубы:");
+        foreach (var cube in cubes)
+        {
+            cube.PrintInfo();
+        }
+
+        Shape[] cylinders = new Shape[]
+        {
+            new Cylinder(2, 4),
+            new Cylinder(3, 5),
+            new Cylinder(1, 3),
+            new Cylinder(4, 6),
+            new Cylinder(5, 7)
+        };
+
+        Array.Sort(cylinders, (c1, c2) => c2.Volume.CompareTo(c1.Volume));
+        Console.WriteLine("Цилиндры:");
+        foreach (var cylinder in cylinders)
+        {
+            cylinder.PrintInfo();
+        }
+
+        Shape[] allShapes = new Shape[spheres.Length + cubes.Length + cylinders.Length];
+        spheres.CopyTo(allShapes, 0);
+        cubes.CopyTo(allShapes, spheres.Length);
+        cylinders.CopyTo(allShapes, spheres.Length + cubes.Length);
+
+        Array.Sort(allShapes, (s1, s2) => s2.Volume.CompareTo(s1.Volume));
+        Console.WriteLine("Все фигуры:");
+        foreach (var shape in allShapes)
+        {
+            shape.PrintInfo();
+        }
+    }
+}
